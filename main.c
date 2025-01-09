ece75a9b5c6242b60f97d29a9c0dc60023b241c6
Ramin Rafiee <redcrossprog@gmail.com>
2025-01-09 17:31:19 +0330

    render table

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define COLS 30
+#define ROWS 15
 
 int main() {
-    printf("Hello from snake!\n");
+    int quit = 0;
+    while (!quit) {
+        // render table 
+        printf("⊢");
+        for (int i = 0; i< COLS; i++) {
+            printf("-");
+        }
+        printf("⊣\n");
+        for (int j = 0; j<ROWS; j++) {
+            printf("|");
+            for (int i = 0; i<COLS; i++){
+                printf(".");
+            }
+            printf("|\n");
+        }
+
+        printf("⊢");
+        for (int i = 0; i< COLS; i++) {
+            printf("-");
+        }
+        printf("⊣\n");
+        
+        printf("\e[%iA", ROWS + 2);
+    }
     return 0;
 }
